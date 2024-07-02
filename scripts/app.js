c32c73d2b8fb7a5ed729907ea8d541b3048f9292
Dan Dyar <dan@parameterz.com>
2024-07-02 05:53:58 -0700

    Update app.js
    removed the asterisk in values greater than 40; added missing units to WASE output

--- scripts/app.js
+++ scripts/app.js
@@ -50,8 +50,8 @@ $(document).ready(function() {
         const rows = sites.map(site => {
             const ulnNORRE = calculateULN(heightCm, 'NORRE', site, gender);
             const ulnWASE = site !== "AAO" ? calculateULN(heightCm, 'WASE', site, gender, ageGroup) : "n/a";
-            const ulnNORREDisplay = ulnNORRE > 40 ? `${ulnNORRE} cm *` : `${ulnNORRE} cm`;
-            const ulnWASEDisplay = ulnWASE > 40 ? `${ulnWASE} cm *` : `${ulnWASE}`;
+            const ulnNORREDisplay = `${ulnNORRE} cm`; // ulnNORRE > 40 ? `${ulnNORRE} cm *` : `${ulnNORRE} cm`;
+            const ulnWASEDisplay = `${ulnWASE} cm`; //ulnWASE > 40 ? `${ulnWASE} cm *` : `${ulnWASE}`;
             return `<tr>
                         <td>${site}</td>
                         <td>${ulnNORREDisplay}</td>
