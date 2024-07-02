81881c129b1d3d489f3c30dde98f395d1545c37e
Dan Dyar <dan@parameterz.com>
2024-07-01 22:44:03 -0700

    added WASE and updated the results table

--- scripts/app.js
+++ scripts/app.js
@@ -1,24 +1,23 @@
 $(document).ready(function() {
     const data = {
         NORRE: {
-        SOV: { female: 22.1, male: 23.2 },
-        STJ: { female: 19.4, male: 19.8 },
-        AAO: { female: 21.1, male: 21.4 }
+            SOV: { female: 22.1, male: 23.2 },
+            STJ: { female: 19.4, male: 19.8 },
+            AAO: { female: 21.1, male: 21.4 }
         },
         WASE: {
             SOV: {
-                "18-40": {female: 22, male: 21.8},
-                "41-65": {female: 22.6, male: 23.2},
-                ">65": {female: 23.9, male: 24.3}
+                "18-40": { female: 22, male: 21.8 },
+                "41-65": { female: 22.6, male: 23.2 },
+                ">65": { female: 23.9, male: 24.3 }
             },
-            STJ : {
-            "18-40": {female: 19.2, male: 19},
-            "41-65": {female: 20.2, male: 20.7},
-            ">65": {female: 21, male: 21.7}
+            STJ: {
+                "18-40": { female: 19.2, male: 19 },
+                "41-65": { female: 20.2, male: 20.7 },
+                ">65": { female: 21, male: 21.7 }
             }
-        },
-    }
-    
+        }
+    };
 
     function generateHeightData(startCm, endCm) {
         const heightData = [];
@@ -35,20 +34,29 @@ $(document).ready(function() {
         return heightData;
     }
 
-    function calculateULN(heightCm, site, gender) {
-        return ((heightCm / 100) * data[site][gender]).toFixed(1);
+    function calculateULN(heightCm, study, site, gender, ageGroup) {
+        console.log(heightCm, study, site, gender, ageGroup);
+        if (study === "NORRE") {
+            return ((heightCm / 100) * data[study][site][gender]).toFixed(1);
+        } else if (study === "WASE" && site !== "AAO") {
+            return ((heightCm / 100) * data[study][site][ageGroup][gender]).toFixed(1);
+        }
+        return "n/a";
     }
 
-    function createTable(heightData, site, gender) {
-        const headers = ['Height (cm)', 'Height (ft & in)', 'Height (in)', 'ULN'];
+    function createTable(heightData, site, gender, ageGroup) {
+        const headers = ['Height (cm)', 'Height (ft & in)', 'Height (in)', 'NORRE', 'WASE'];
         const rows = heightData.map(entry => {
-            const uln = calculateULN(entry.cm, site, gender);
-            const ulnDisplay = uln > 40 ? `${uln} cm *` : `${uln} cm`;
+            const ulnNORRE = calculateULN(entry.cm, 'NORRE', site, gender);
+            const ulnWASE = calculateULN(entry.cm, 'WASE', site, gender, ageGroup);
+            const ulnNORREDisplay = ulnNORRE > 40 ? `${ulnNORRE} cm *` : `${ulnNORRE} cm`;
+            const ulnWASEDisplay = ulnWASE > 40 ? `${ulnWASE} cm *` : `${ulnWASE}`;
             return `<tr>
                         <td>${entry.cm} cm</td>
                         <td>${entry.feet}' ${entry.inches}"</td>
                         <td>${(entry.cm / 2.54).toFixed(1)} in</td>
-                        <td>${ulnDisplay}</td>
+                        <td>${ulnNORREDisplay}</td>
+                        <td>${ulnWASEDisplay}</td>
                     </tr>`;
         }).join('');
         return `
@@ -56,7 +64,8 @@ $(document).ready(function() {
                 <thead>
                     <tr>
                         <th colspan="3">Height</th>
-                        <th>ULN (${site})</th>
+                        <th colspan="2">ULN (${site})</th>
+                        
                     </tr>
                     <tr>
                         ${headers.map(header => `<th>${header}</th>`).join('')}
@@ -69,18 +78,29 @@ $(document).ready(function() {
         `;
     }
 
-    function scrollToResults() {
-        const resultsSection = document.getElementById('results');
-        resultsSection.scrollIntoView({ behavior: 'smooth' });
-    }
+    $('#site').on('change', function() {
+        const selectedSite = $(this).val();
+        if (selectedSite === 'SOV' || selectedSite === 'STJ') {
+            $('#ageGroup, #ageGroupLabel').show();
+        } else {
+            $('#ageGroup, #ageGroupLabel').hide();
+        }
+    });
 
     $('#aortaForm').on('submit', function(event) {
         event.preventDefault();
         const gender = $('#gender').val();
         const site = $('#site').val();
+        const ageGroup = $('#ageGroup').val() || '18-40'; // default age group if not applicable
         const heightData = generateHeightData(140, 210);
-        const resultsTable = createTable(heightData, site, gender);
+        const resultsTable = createTable(heightData, site, gender, ageGroup);
         $('#resultsContainer').html(resultsTable);
+        $('#resultsTitle').html(`Results: (${site})`);
         scrollToResults();
     });
+
+    function scrollToResults() {
+        const resultsSection = document.getElementById('results');
+        resultsSection.scrollIntoView({ behavior: 'smooth' });
+    }
 });
